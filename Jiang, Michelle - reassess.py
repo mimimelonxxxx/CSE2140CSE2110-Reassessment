0eab687812a52db00633247a6290bcb97c68c76d
Michelle Jiang <m.jiang4@share.epsb.ca>
2023-01-17 09:00:48 -0700

    started file

---
+++ Jiang, Michelle - reassess.py
@@ -0,0 +1,24 @@
+"""
+title: 
+author: Michelle Jiang 
+date-created: 2023-01-17
+"""
+
+### FUNCTIONS ### 
+
+# INPUTS # 
+
+# PROCESSING # 
+
+# OUTPUTS # 
+
+### MAIN PROGRAM CODE ### 
+
+if __name__ == "__main__":
+    pass
+    # INPUTS # 
+
+    # PROCESSING # 
+
+    # OUTPUTS # 
+    
